38a4036a0a091593f8a57bfec0e3d39b0f382437
eginatakasan <eginatakasan@gmail.com>
2023-12-20 02:08:32 +0700

    feat: update linkedin url

--- src/constants/url.constant.ts
+++ src/constants/url.constant.ts
@@ -3,8 +3,8 @@ import env from './env.constant'
 const RESUME_URL = `${env.baseUrl}/resume.pdf`
 
 const MAIL_URL = 'mailto:eginatakasan@gmail.com'
-const LINKEDIN_URL = 'https://www.linkedin.com/in/eginata-kasan-74a72516b'
-const GITHUB_URL = 'https://github.com/eginatakasan'
+const LINKEDIN_URL = 'https://www.linkedin.com/in/eginata-kasan/'
+const GITHUB_URL = 'https://github.com/eginatakasan/'
 
 const url = {
   resume: RESUME_URL,
