c2778e95e3b7889bb4db5e876d23a948d92d69f6
eginatakasan <eginatakasan@gmail.com>
2025-07-25 18:17:42 +1000

    Add a button to github io repo

--- src/components/organisms/Introduction.tsx
+++ src/components/organisms/Introduction.tsx
@@ -3,6 +3,7 @@ import url from '../../constants/url.constant';
 import { ArrowDownTrayIcon } from '@heroicons/react/24/solid';
 import { Button } from '../atoms';
 import AnimatedName from './AnimatedName';
+import Github from '../atoms/Icon/Github.icon';
 
 const IntroductionView = () => {
   return (
@@ -22,24 +23,39 @@ const IntroductionView = () => {
           Adept in Web & Mobile App development. I'm always working varying
           projects as a part-time Full-stack developer at QSStudio, Sydney. My
           past experience as a UX Engineer means I specialize in building
-          precise UI with detailed micro-interactions to better user experience
+          precise UI with detailed micro-interactions to improve user experience
           across products.
         </div>
         <p className="text-sm mt-2 italic font-raleway text-textBlack">
           I also go by "Eggy"
         </p>
-        <a
-          href={url.resume}
-          target="_blank"
-          rel="noopener noreferrer"
-          download="Eginata Kasan's Resume.pdf"
-          aria-label="Download Eginata Kasan's Resume"
-        >
-          <Button className="mt-16 font-light">
-            <ArrowDownTrayIcon className="w-4 h-4" aria-hidden="true" />
-            Check out my Resume
-          </Button>
-        </a>
+
+        <div className="flex flex-row space-x-4">
+          <a
+            href={url.resume}
+            target="_blank"
+            rel="noopener noreferrer"
+            download="Eginata Kasan's Resume.pdf"
+            aria-label="Download Eginata Kasan's Resume"
+          >
+            <Button className="mt-16 font-light">
+              <ArrowDownTrayIcon className="w-5 h-5" aria-hidden="true" />
+              Check out my Resume
+            </Button>
+          </a>
+
+          <a
+            href={url.githubIoRepo}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="See How I Made this Website"
+          >
+            <Button variant="outlined" className=" mt-16 font-light">
+              <Github className="w-5 h-5" aria-hidden="true" />
+              See Website Code
+            </Button>
+          </a>
+        </div>
       </div>
     </section>
   );
--- src/constants/url.constant.ts
+++ src/constants/url.constant.ts
@@ -3,12 +3,14 @@ const RESUME_URL = `${process.env.REACT_APP_BASE_URL}/resume.pdf`
 const MAIL_URL = 'mailto:eginatakasan@gmail.com'
 const LINKEDIN_URL = 'https://www.linkedin.com/in/eginata-kasan/'
 const GITHUB_URL = 'https://github.com/eginatakasan/'
+const GITHUB_IO_REPO_URL = 'https://github.com/eginatakasan/eginatakasan.github.io'
 
 const url = {
   resume: RESUME_URL,
   mail: MAIL_URL,
   linkedin: LINKEDIN_URL,
   github: GITHUB_URL,
+  githubIoRepo: GITHUB_IO_REPO_URL,
 }
 
 export default url
